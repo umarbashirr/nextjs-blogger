ea7efb61a1f6876df4a035398b5ea7a53e9c9068
umarbashirr <umarbashir93@outlook.com>
2024-11-05 17:32:05 +0530

    public posts archive page and single post pages initialized

---
+++ src/app/(website)/(routes)/posts/[postId]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PublicSinglePostPage = () => {
+  return <div>PublicSinglePostPage</div>;
+};
+
+export default PublicSinglePostPage;
---
+++ src/app/(website)/(routes)/posts/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PublicPostsPage = () => {
+  return <div>PublicPostsPage</div>;
+};
+
+export default PublicPostsPage;
