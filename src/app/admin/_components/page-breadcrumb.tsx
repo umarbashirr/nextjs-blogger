4201ca3d12a7e4b3451b6c1db5ec3d2ef22f7946
umarbashirr <umarbashir93@outlook.com>
2024-11-05 17:30:31 +0530

    dynamic breadcrumb created for admin panel

---
+++ src/app/admin/_components/page-breadcrumb.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { usePathname } from "next/navigation";
+import { adminMenuItems } from "@/utils/admin-menu";
+
+export const PageBreadCrumb = () => {
+  const pathname = usePathname();
+  const [matchedItemsForBreadcrumb, setMatchedItemsForBreadcrumb] = useState<
+    any[]
+  >([]);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    // Set mounted state to true to avoid SSR issues
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      const paths = pathname.split("/").slice(1);
+      const items = adminMenuItems;
+
+      let matchedItemsForBreadcrumb: any[] = [];
+
+      paths.forEach((path) => {
+        const matchedItem = items.find((item) => item.url.includes(path));
+        if (matchedItem) {
+          matchedItemsForBreadcrumb.push(matchedItem); // Only push if matchedItem is not undefined
+        }
+      });
+
+      setMatchedItemsForBreadcrumb(matchedItemsForBreadcrumb);
+    }
+  }, [pathname, isMounted]);
+
+  // Avoid rendering until the component is fully mounted
+  if (!isMounted) return null;
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {matchedItemsForBreadcrumb.map((item, index) => (
+          <div key={item?.title} className="flex items-center gap-2">
+            <BreadcrumbItem>
+              <BreadcrumbLink href={item?.url}>{item?.title}</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator
+              className={
+                index === matchedItemsForBreadcrumb.length - 1 ? "hidden" : ""
+              }
+            />
+          </div>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
